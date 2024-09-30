cba5ed29bc88e997c770b7f4a2cad0496f0399a1
shifa <nuriyahshifaaun@gmail.com>
2024-09-30 15:10:14 +0700

    Shifa Persegi baru

--- src/bangun_datar/Persegi.java
+++ src/bangun_datar/Persegi.java
@@ -1,9 +1,5 @@
 package bangun_datar;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
