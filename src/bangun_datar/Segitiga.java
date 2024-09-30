f8bae52aa08e453c0e872d35e941603b64e436d9
yusufadi <dragonangun@gmail.com>
2024-09-30 12:19:00 +0700

    YusupAdi(Segitiga)

---
+++ src/bangun_datar/Segitiga.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bangun_datar;
+
+/**
+ *
+ * @author MyBook Hype
+ */
+public class Segitiga {
+    double Sisi, alas, tinggi;
+    double luas, keliling;
+    public void setSisi(double sisi){
+    this.Sisi = sisi;
+    }
+    public void setAlas(double alas){
+    this.alas = alas;
+    }
+    public void setTinggi(double tinggi){
+    this.tinggi = tinggi;
+    
+}
+    public void setLuas(){
+        this.luas = 0.5 * alas * tinggi;
+    }
+    public void setKeliling(){
+        this.keliling = Sisi+Sisi+Sisi;
+        
+    }
+    public double getLuas(){
+     return luas;
+    }
+    public double getKeliling(){
+        return keliling;
+    }
+}
